516faf6c882c0c098c0260cbf2822ef05a399281
admin <admin@f5lab.local>
2020-07-27 13:19:01 -0400

    address checkout name change

--- powershell/ad_api/endpoints/POST/checkout-address.ps1
+++ powershell/ad_api/endpoints/POST/checkout-address.ps1
@@ -2,7 +2,7 @@
 	.DESCRIPTION
 		This script reserves an IP addreess in the DHCP server usinig a fake mac address
 	.EXAMPLE
-        .\checkout-address.ps1 -Body '{"scope":"10.1.10.0", "address":"10.1.10.105", "vs_name":"test.acme.com"}'
+        .\checkout-address.ps1 -Body '{"scope":"10.1.10.0", "address":"10.1.10.105", "name":"test.acme.com"}'
 	.NOTES
         This will return a IP address assigned
 #>
@@ -19,11 +19,11 @@ $newbody = $body | ConvertFrom-Json
 
 
 $scope = $newbody.scope
-#Write-Host " Scope: $scope"
+#Write-Host "Scope: $scope"
 $address = $newbody.address
-#Write-Host " IP address: $address"
-$name = $newbody.vs_name
-#Write-Host " VS Name: $name"
+#Write-Host "IP address: $address"
+$name = $newbody.name
+#Write-Host "Name: $name"
 
  $mac = (0..5 | ForEach-Object { '{0:x}{1:x}' -f (Get-Random -Minimum 0 -Maximum 15),(Get-Random -Minimum 0 -Maximum 15)})  -join '-'
 
@@ -35,9 +35,9 @@ $name = $newbody.vs_name
     $jsonresponse = New-Object -TypeName psobject
     $jsonresponse| Add-Member -MemberType NoteProperty -Name status -Value $status
     $jsonresponse| Add-Member -MemberType NoteProperty -Name address -Value $address
-    $jsonresponse| Add-Member -MemberType NoteProperty -Name vs_name -Value $name
+    $jsonresponse| Add-Member -MemberType NoteProperty -Name name -Value $name
  
- 	$Message = $jsonresponse | Select-Object status, address, vs_name
+ 	$Message = $jsonresponse | Select-Object status, address, name
  
     
  } else {
@@ -47,9 +47,9 @@ $name = $newbody.vs_name
  $jsonresponse = New-Object -TypeName psobject
  $jsonresponse| Add-Member -MemberType NoteProperty -Name status -Value $status
  $jsonresponse| Add-Member -MemberType NoteProperty -Name address -Value $address
- $jsonresponse| Add-Member -MemberType NoteProperty -Name vs_name -Value $name
+ $jsonresponse| Add-Member -MemberType NoteProperty -Name name -Value $name
  
- 	$Message = $jsonresponse | Select-Object status, address, vs_name
+ 	$Message = $jsonresponse | Select-Object status, address, name
  }
  
  return $message
