af604126d630051387bad3fe7a9c01c55ae23462
lesleycw <i@chas.one>
2021-02-04 08:04:08 -0800

    v1.22
    DNS host issue

--- containers/tao-portal-build/addhosts.sh
+++ containers/tao-portal-build/addhosts.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "172.17.0.1 admportal.f5lab.local" >> /etc/hosts
 echo "10.1.20.9 files-dev.f5lab.local" >> /etc/hosts
-echo "10.1.20.9 fils-master.f5lab.local" >> /etc/hosts
+echo "10.1.20.9 files-master.f5lab.local" >> /etc/hosts
